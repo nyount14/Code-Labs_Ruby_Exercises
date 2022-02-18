db2fd6d9463eca710a9067990773815cb394a2ca
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-02-17 20:03:12 -0600

    Create exercise-1.4.rb

---
+++ part-1-exercises/exercise-1.4.rb
@@ -0,0 +1,9 @@
+pet_1 = "Zena"
+pet_2 = "Boot"
+pet_3 = "Bean"
+
+def names_of_my_pets(name_1, name_2, name_3)
+    puts "I have 3 pets!  Their names are #{name_1}, #{name_2}, and #{name_3}"
+end
+
+names_of_my_pets(pet_1, pet_2, pet_3)
