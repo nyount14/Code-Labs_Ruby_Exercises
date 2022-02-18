8961a69c41a8b25f543f2fee9dd8759959ede37f
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-02-17 19:52:34 -0600

    Create exercise-1.3.rb

---
+++ part-1-exercises/exercise-1.3.rb
@@ -0,0 +1,32 @@
+def favorite_movie(movie)
+    print "My favorite movie is #{movie}"
+end
+
+favorite_movie("Office Space")
+
+puts
+
+def favorite_food(food)
+    print "My favorite food is #{food}"
+end
+
+favorite_food("Shwarma")
+
+puts
+
+def favorite_drink(drink)
+    print "My favorite drink is #{drink}"
+end 
+
+favorite_drink("tea with milk")
+
+puts
+
+def list_of_favorite_things(movie, food, drink)
+    puts favorite_movie(movie)
+    puts favorite_food(food)
+    puts favorite_drink(drink)
+
+end
+
+list_of_favorite_things("Shawshank Redemption", "pb and J", "coffee")
