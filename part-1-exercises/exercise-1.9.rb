8affbaae70465c71643a1827e2b6b3ca6f9165c0
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-02-20 12:10:29 -0600

    Create exercise-1.9.rb

---
+++ part-1-exercises/exercise-1.9.rb
@@ -0,0 +1,22 @@
+def add(num1, num2)
+    num1 + num2
+end
+
+def subtract(num1, num2)
+    num1 - num2
+end
+
+def multiply(num1, num2)
+    num1 * num2
+end
+
+def divide(num1, num2)
+    num1 / num2
+end
+
+puts add(1, 2)
+puts subtract(70, 9)
+puts multiply(7, 3)
+puts divide(5, 1)
+
+
