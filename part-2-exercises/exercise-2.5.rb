1b5b84c7f9044654616549417ece204391f20390
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-02-21 20:25:26 -0600

    Create exercise-2.5.rb

---
+++ part-2-exercises/exercise-2.5.rb
@@ -0,0 +1,20 @@
+users = [
+    {
+        name: "John Doe",
+        age: 43
+    },
+    {
+        name: "Amy Singer",
+        age: 53
+    },
+    {
+        name: "Jimmy Lendricks",
+        nage: 23
+    }
+]
+
+
+
+while !users.has_value?("Jimmy Lendricks")
+    puts "test"
+end
