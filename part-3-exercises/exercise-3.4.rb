2aff0ad914a76f400c0bd70709c6a6523c0190ab
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-02-25 18:35:13 -0600

    Update exercise-3.4.rb

--- part-3-exercises/exercise-3.4.rb
+++ part-3-exercises/exercise-3.4.rb
@@ -1,24 +1,23 @@
 class Person
 
     @@people = []
-
-    attr_accessor :name, :age
-
-    def initialize(name, age)
-        @name = name
-        @age = age
-        @@people.push(self)
-    end
-
-    def print_all_people(array)
-        array.each do |element|
-            puts element
-        end
-    endg
     
-end
-
-person1 = Person.new("Jill", "30")
-person2 = Person.new("Frank", "45")
-person3 = Person.new("Sally", "40")
-print_all_people(@@people)
+    attr_accessor :name, :age  
+  
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@people.push(self)
+    end
+  
+    def self.print_all_people
+          @@people.each do |element|
+              puts element
+          end
+    end
+  end
+  
+  person1 = Person.new("Jill", 30)
+  person2 = Person.new("Frank", 45)
+  person3 = Person.new("Sally", 40)
+  p Person.print_all_people
