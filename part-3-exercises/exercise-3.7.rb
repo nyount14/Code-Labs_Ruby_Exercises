37134e7fe78b44c9f3a6f27ef3a0167d3c9d9010
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-02-27 17:07:12 -0600

    Update exercise-3.7.rb

--- part-3-exercises/exercise-3.7.rb
+++ part-3-exercises/exercise-3.7.rb
@@ -1,11 +1,27 @@
-def sum(num1, num2)
-    result = (num1 + num2).chars
-    result1 = result.shift(2)
-    result.slice!(1)
-    result.push("4")
-    result.join
-
+def add_strings(num1, num2)
+    i = num1.length - 1
+    j = num2.length - 1
+    sol = ''
+    carry = 0
+    while i >= 0 || j >= 0 || carry > 0
+        sum = carry
+        if i >= 0
+            sum += num1[i].ord - '0'.ord
+            i -= 1
+        end
+        if j >= 0
+            sum += num2[j].ord - '0'.ord
+            j -= 1
+        end
+        carry = sum/10
+        sol = String(sum%10) + sol
+    end
+    
+    sol
 end
 
-puts sum("11", "123")
+p add_strings("500","499")
+
+
+    
 
